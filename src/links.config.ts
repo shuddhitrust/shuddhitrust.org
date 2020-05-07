70c5d635d8bed0e2f8d63c00a0d3816c29caefe4
ryarasi <ragav.code@gmail.com>
2020-05-07 14:54:46 +0530

    Update links.config.ts
    M video descriptions brief

--- src/links.config.ts
+++ src/links.config.ts
@@ -1,6 +1,6 @@
 export const videos = [
-    { id: "saU8WohDQiA", title: "Introducing Shuddhi Vidhya", description: "Why is it important to critically examine education? What is our vision for education?" },
-    { id: "ddJJHn-5ntI", title: "Shuddhi Fund 2030 Mandate", description: "Our agendas for the organisation for the next 10 years, until 2030." },
+    { id: "saU8WohDQiA", title: "Introducing Shuddhi Vidhya", description: "Why is it important to critically examine education? What is our vision for it?" },
+    { id: "ddJJHn-5ntI", title: "Shuddhi Fund 2030 Mandate", description: "Our agendas for the next 10 years, until 2030." },
     { id: "aisF-bJ9ELg", title: "Why Shuddhi Fund?", description: "What is Shuddhi Fund? Why did we set it up?" }
 ]
 
