67f6ecd2297e398d24519f18a73a96a98c39f91c
ryarasi <ragav.code@gmail.com>
2020-05-20 18:55:03 +0530

    Rearranged the order of the videos

--- src/links.config.ts
+++ src/links.config.ts
@@ -1,8 +1,9 @@
+// Latest videos at the top
 export const videos = [
+    { id: "algbtcpKevw", title: "Revolutionising Education: Part 1", description: "What are some of the problems in our approach to education today?" },
     { id: "saU8WohDQiA", title: "Introducing Shuddhi Vidhya", description: "Why is it important to critically examine education? What is our vision for it?" },
     { id: "ddJJHn-5ntI", title: "Shuddhi Fund 2030 Mandate", description: "Our agendas for the next 10 years, until 2030." },
-    { id: "aisF-bJ9ELg", title: "Why Shuddhi Fund?", description: "What is Shuddhi Fund? Why did we set it up?" },
-    { id: "algbtcpKevw", title: "Revolutionising Education: Part 1", description: "What are some of the problems in our approach to education today?" }
+    { id: "aisF-bJ9ELg", title: "Why Shuddhi Fund?", description: "What is Shuddhi Fund? Why did we set it up?" }
 ]
 
 export const twitter = "https://twitter.com/shuddhifund"
