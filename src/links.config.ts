690e4626e129a059c79b970676dec7a7335afdfa
ryarasi <ragav.code@gmail.com>
2020-08-14 17:12:17 +0530

    Added latest video for August 2020

--- src/links.config.ts
+++ src/links.config.ts
@@ -1,5 +1,6 @@
 // Latest videos at the top
 export const videos = [
+    { id: "tisvb-QsVlg", title: "Exciting updates on collaboration with Isha Vidhya!", description: "In this video I speak about an exciting new initiative we've launched as part of our collaboration with Isha Vidhya. There's a nice backstory for it too and photos/videos to accompany it too!"},
     { id: "34ooDS-0R6s", title: "Shuddhi Vidhya Updates: Isha Vidhya Collaboration & Our new YouTube Channels!", description: "In this video I speak about a couple of updates regarding our efforts with Shuddhi Vidhya as of July 2020." },
     { id: "SDaGO4GvsqA", title: "Revolutionising Education: Part 2", description: "A discussion on how we can solve the problems in our approach to education." },
     { id: "algbtcpKevw", title: "Revolutionising Education: Part 1", description: "What are some of the problems in our approach to education today?" },
