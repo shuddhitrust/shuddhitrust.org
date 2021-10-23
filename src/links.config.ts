e01bc63ac42aa7a90fe637769f25ad2d30de0f0f
ragavyarasi <ragav.code@gmail.com>
2021-10-23 22:01:59 +0530

    Using razorpay for donation now

--- src/links.config.ts
+++ src/links.config.ts
@@ -144,5 +144,4 @@ export const medium = 'https://medium.com/shuddhitrust';
 export const shuddhiVidhya = 'https://shuddhividhya.org';
 export const volunteer =
   'https://docs.google.com/forms/d/e/1FAIpQLSfOctV_9Tz0zi25xlK2N13n0pYqN13iRhEI3um03kBCQciUiw/viewform?usp=sf_link';
-export const donation =
-  'https://docs.google.com/forms/d/e/1FAIpQLSe-RvNUN3wDFm_Tzh1l7OAkOU7Wjz6yPLtFGbEr21dIQCx4yw/viewform?usp=sf_link';
+export const donation = 'https://rzp.io/l/shuddhitrust';
